322773d5c6bb0f5c200943f6c38006ac9265a05b
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2025-06-15 07:08:36 +0900

    Update mdc.rb

--- mdc.rb
+++ mdc.rb
@@ -1,8 +1,8 @@
 class Mdc < Formula
   desc "Simple markdown viewer built with Tauri"
-  homepage "https://github.com/yusuke-nozoe/mdc"
-  url "https://github.com/yusuke-nozoe/mdc/releases/download/v0.1.1/mdc-aarch64-apple-darwin"
-  sha256 "36ab4fe67e9b14a89cce42eb5230e9e20c821f7674a39fe3f1fcc33aa3d4d630"
+  homepage "https://github.com/zoetin45/mdc"
+  url "https://github.com/zoetin45/mdc/releases/download/v0.1.0/mdc-aarch64-apple-darwin"
+  sha256 "081675f3268611b78cf4e4994fc40e17b527ee14f972d73ef775674cc9c20e2e"
   license "MIT"
 
   def install
