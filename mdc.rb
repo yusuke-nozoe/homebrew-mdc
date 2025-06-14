bd34797325a2afdbb5740a558337d926211b0537
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2025-06-14 16:59:08 +0900

    Update mdc.rb

--- mdc.rb
+++ mdc.rb
@@ -1,21 +1,21 @@
 class Mdc < Formula
-    desc "Simple markdown viewer built with Tauri"
-    homepage "https://github.com/yusuke-nozoe/mdc"
-    url "https://github.com/yusuke-nozoe/mdc/archive/v0.1.0.tar.gz"
-    sha256 "081675f3268611b78cf4e4994fc40e17b527ee14f972d73ef775674cc9c20e2e"
-    license "MIT"
+  desc "Simple markdown viewer built with Tauri"
+  homepage "https://github.com/yusuke-nozoe/mdc"
+  url "https://github.com/yusuke-nozoe/mdc/archive/v0.1.0.tar.gz"
+  sha256 "081675f3268611b78cf4e4994fc40e17b527ee14f972d73ef775674cc9c20e2e"
+  license "MIT"
 
-    depends_on "rust" => :build
-    depends_on "node" => :build
-    depends_on "pnpm" => :build
+  depends_on "rust" => :build
+  depends_on "node" => :build
+  depends_on "pnpm" => :build
 
-    def install
-      cd "src-tauri" do
-        system "cargo", "install", "--locked", "--root", prefix, "--path", "."
-      end
-    end
-
-    test do
-      system "#{bin}/mdc", "--version"
+  def install
+    cd "src-tauri" do
+      system "cargo", "install", "--locked", "--root", prefix, "--path", "."
     end
   end
+
+  test do
+    system "#{bin}/mdc", "--version"
+  end
+end
