0a0d9c47c3e467324a51d73276e794409242894b
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2025-06-14 17:55:00 +0900

    Update mdc.rb

--- mdc.rb
+++ mdc.rb
@@ -1,25 +1,12 @@
 class Mdc < Formula
   desc "Simple markdown viewer built with Tauri"
   homepage "https://github.com/yusuke-nozoe/mdc"
-  url "https://github.com/yusuke-nozoe/mdc/archive/v0.1.1.tar.gz"
+  url "https://github.com/yusuke-nozoe/mdc/releases/download/v0.1.1/mdc-aarch64-apple-darwin"
   sha256 "00573ee769da9ff060346335d183be872bd4e9f5d8341a67a5a8f476b6eafbe9"
   license "MIT"
 
-  depends_on "rust" => :build
-  depends_on "node" => :build
-  depends_on "pnpm" => :build
-
   def install
-    # フロントエンドのビルド
-    system "pnpm", "install"
-    system "pnpm", "build"
-
-    cd "src-tauri" do
-      # tauri.conf.jsonでDMGを無効化
-      system "cargo", "tauri", "build", "--target", "aarch64-apple-darwin", "--bundles", "none"
-    end
-    # 生成されたバイナリをインストール
-    bin.install "src-tauri/target/aarch64-apple-darwin/release/mdc"
+    bin.install "mdc-aarch64-apple-darwin" => "mdc"
   end
 
   test do
